2a46fb816012bf3c8b305b4fa199e82a6f2497d4
Michael Hernandez <gammabeta010@gmail.com>
2020-02-25 15:40:48 +0100

    Verificamo se l'utente ha inserito un numero da 1 a 100.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 var numberList = []
 var listLength = 16;
+var userInput;
+const regExp = '^[0-9]+$';
+const inputRangeMin = 1;
+const inputRangeMax = 100;
 
 console.log("Before generating list, list has: " + numberList);
 generateRandomNumberList();
 
 console.log(numberList);
+
+
+
+do {
+    userInput = prompt("Please enter a number from 1 to 100");
+    console.log("userInput is: " + userInput);
+} while (checkUserInput(userInput, inputRangeMin, inputRangeMax));
+
+console.log("user entered: " + userInput);
 
 
 function generateRandomNumberList () {
@@ -30,9 +43,18 @@ function generateRandomNumberList () {
         console.log(numberList[i] + " current position: " + i);
     }
 }
+function checkUserInput (value, min, max) {
 
-function checkUserInput (input) {
+    var keepPrompting;
 
+    if (value.match(regExp) === null || !(value >= min && value <= max)) {
+        keepPrompting = true;
+    }
+    else {
+        keepPrompting = false;
+    }
+
+    return keepPrompting;
 }
 
 function isNumberInList (number) {
